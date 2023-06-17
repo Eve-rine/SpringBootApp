49c0d314df84d78d6b618734b58a32e42064a63e
Evrine <minayoevrine@gmail.com>
2023-06-17 19:08:14 +0300

    connecting to mysql database

--- src/main/java/users/controllers/UsersController.java
+++ src/main/java/users/controllers/UsersController.java
@@ -2,14 +2,14 @@ package users.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import users.models.Users;
+import users.models.User;
 
 import java.util.List;
 
 @RestController
 public class UsersController {
     @GetMapping ("/users")
-    List<Users> all() {
-        return List.of(new Users(1, "John", "admin"), new Users(2,"Jane", "user"));
+    List<User> all() {
+        return List.of(new User(1L, "John", "admin"), new User(2L,"Jane", "user"));
     }
 }
