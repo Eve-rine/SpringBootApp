9711e3cbd735b3666a7ccb1d4a7eaff55c571c46
Evrine <minayoevrine@gmail.com>
2023-06-17 20:10:43 +0300

    creating crud rest apis

--- src/main/java/users/models/User.java
+++ src/main/java/users/models/User.java
@@ -11,13 +11,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
     private  String email;
-    public User(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
+//    public User(Long id, String name, String email) {
+//        this.id = id;
+//        this.name = name;
+//        this.email = email;
+//    }
 
     public Long getId() {
         return this.id;
--- src/main/java/users/repositories/UserRepository.java
+++ src/main/java/users/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package users.repositories;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import users.models.User;
+
+public interface UserRepository extends CrudRepository<User,Integer> {
+
 }
