9dc36fb1bb9d673197f4b787de1159f69fa323d1
Evrine <minayoevrine@gmail.com>
2023-07-17 13:40:58 +0300

    adding auth to user service

--- src/main/java/users/WebSecurityConfig.java
+++ src/main/java/users/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
     @EnableWebSecurity
     public class WebSecurityConfig {
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
--- src/main/java/users/controllers/auth/AuthController.java
+++ src/main/java/users/controllers/auth/AuthController.java
@@ -1,4 +1,51 @@
 package users.controllers.auth;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 public class AuthController {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpirationMs;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Validated @RequestBody LoginRequest loginRequest) {
+        // Authenticate the user (e.g., check username and password)
+
+        // Generate JWT token
+        String token = generateJwtToken(loginRequest.getUsername());
+
+        // Return the token in the response
+        return ResponseEntity.ok(token);
+    }
+
+    private String generateJwtToken(String username) {
+        Key signingKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime expirationTime = currentTime.plusSeconds(jwtExpirationMs / 1000);
+
+        Date issuedAt = Date.from(currentTime.atZone(ZoneId.systemDefault()).toInstant());
+        Date expirationDate = Date.from(expirationTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expirationDate)
+                .signWith(signingKey)
+                .compact();
+    }
 }
